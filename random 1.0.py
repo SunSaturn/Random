5b20fc55d6a7c3f0c0bfa9147cab64bae4125086
SunSaturn <44498012+SunSaturn@users.noreply.github.com>
2018-11-03 17:30:13 +0400

    Add files via upload

---
+++ random 1.0.py
@@ -0,0 +1,6 @@
+import random
+X=random.randint(100,999)
+A=X//100
+Y=X%100//10
+B=X%10
+print(X, A, Y, B)
---
+++ random 1.1.py
@@ -0,0 +1,4 @@
+import random
+X=random.randint(100,999)
+print(X)
+print(X//100, ",", X%100//10, ",", X%10, sep="")
